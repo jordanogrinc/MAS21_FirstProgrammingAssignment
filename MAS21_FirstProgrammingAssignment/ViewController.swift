2ff1d0baa9b8a374958a446ac2a3230fe547d950
Jordan <jordanogrinc@gmail.com>
2021-09-08 17:15:09 -0400

    Button working to change text on push

--- MAS21_FirstProgrammingAssignment/ViewController.swift
+++ MAS21_FirstProgrammingAssignment/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var thirdButton: UIButton!
+    
+    @IBOutlet weak var LoginText: UILabel!
+    
+    @IBOutlet weak var AppTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func HelloButtonPush(_ sender: Any) {
+        LoginText.text = "good job"
+    }
+    
 }
 
