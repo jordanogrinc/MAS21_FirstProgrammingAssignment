a154ceb2734adb200bdb86d5c72253bd329e0a3a
Jordan <jordanogrinc@gmail.com>
2021-09-11 14:16:14 -0400

    Home Screen Title and Button

--- MAS21_FirstProgrammingAssignment/ViewController.swift
+++ MAS21_FirstProgrammingAssignment/ViewController.swift
@@ -9,22 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var HomeBtn: UIButton!
     
-    @IBOutlet weak var thirdButton: UIButton!
+    //@IBOutlet weak var thirdButton: UIButton!
     
-    @IBOutlet weak var LoginText: UILabel!
+    //@IBOutlet weak var LoginText: UILabel!
     
-    @IBOutlet weak var AppTitle: UILabel!
+    //@IBOutlet weak var AppTitle: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        HomeBtn.contentEdgeInsets = UIEdgeInsets(top: 8,left: 8,bottom: 8,right: 8);
+        HomeBtn.layer.cornerRadius = 10
+        HomeBtn.layer.borderWidth = 1
+        HomeBtn.layer.borderColor = UIColor.black.cgColor
     }
     
 
-    @IBAction func HelloButtonPush(_ sender: Any) {
+    /*@IBAction func HelloButtonPush(_ sender: Any) {
         LoginText.text = "good job"
     }
+ */
     
 }
 
